5220c6e46dcb77d57fe01e2c8cf33f4f2d0aa2ff
Pete <github@kthxbye.us>
2021-04-17 19:20:00 +0800

    FIX: user docker.io instead of docker package as its wrong

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # download requirements
-sudo apt update && apt install docker docker-compose wget curl -y
+sudo apt update && apt install docker.io docker-compose wget curl -y
 wget https://github.com/multiarch/qemu-user-static/releases/download/v5.2.0-2/qemu-s390x-static
 sudo docker build --build-arg TARGET_ARCH=s390x --build-arg QEMU_ARCH=s390x --build-arg CERTBOT_VERSION=1.13.0 -t certbot/certbot ./
